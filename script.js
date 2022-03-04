34b61b301434e06dd5a978098046ac65ec1f3000
MuraFederico <federicomura98@gmail.com>
2022-03-04 14:24:43 +0100

    aggiunto timer con scomparsa numeri

--- script.js
+++ script.js
@@ -8,9 +8,19 @@ const numbers = [];
 
 for (let i = 0; i < 5; i++) {
     numbers.push(Math.floor(Math.random() * 20 + 1));
-    const number = document.createElement('div');
-    number.innerHTML = numbers[i];
-    container.append(number);
+    const numberElements = document.createElement('div');
+    numberElements.innerHTML = numbers[i];
+    container.append(numberElements);
 }
 
 console.log(numbers)
+
+setTimeout(hideNumbers, 30000);
+
+
+
+function hideNumbers() {
+    const numberElements = document.querySelectorAll('.container > div');
+    numberElements.forEach(numberElements => numberElements.classList.add('hide'));
+
+}
