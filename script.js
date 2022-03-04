a1d0c834640b1ac1ca1fbe99704560e398c114c3
MuraFederico <federicomura98@gmail.com>
2022-03-04 14:39:15 +0100

    aggiunta richiesta e controllo dei numeri con punteggio

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 const container = document.querySelector('.container');
+const output = document.querySelector('.output');
 
 
+const arrNumbers = [];
+const AMOUNT_OF_NUMBERS = 5;
+let score = 0;
 
-const numbers = [];
+/////////// GENERAZIONE NUMERI ///////////
 
-
-
-for (let i = 0; i < 5; i++) {
-    numbers.push(Math.floor(Math.random() * 20 + 1));
+for (let i = 0; i < AMOUNT_OF_NUMBERS; i++) {
+    arrNumbers.push(Math.floor(Math.random() * 20 + 1));
     const numberElements = document.createElement('div');
-    numberElements.innerHTML = numbers[i];
+    numberElements.innerHTML = arrNumbers[i];
     container.append(numberElements);
 }
 
-console.log(numbers)
+console.log(arrNumbers)
 
-setTimeout(hideNumbers, 30000);
+setTimeout(delayedActions, 30000);
 
 
 
-function hideNumbers() {
+function delayedActions() {
+    /////////// RIMOZIONE NUMERI DALLA PAGINA ///////////
     const numberElements = document.querySelectorAll('.container > div');
     numberElements.forEach(numberElements => numberElements.classList.add('hide'));
 
+    /////////// RICHIESTA NUMERI E CONTROLLO ///////////
+
+    for (let i = 0; i < AMOUNT_OF_NUMBERS; i++) {
+        
+        const userNumber = parseInt(prompt('inserisci un numero alla volta'));
+        if (userNumber == arrNumbers[i]) {
+            score += 1;
+        }
+    }
+    output.innerHTML = (`hai ricordato correttamente ${score} numeri`)
 }
